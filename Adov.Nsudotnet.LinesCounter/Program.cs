96d5e4fabcac2c5f99dec7a6f32388f07379e727
Dmitriy Adov <36287211+dmitriyadov@users.noreply.github.com>
2018-05-26 03:17:52 +0700

    only "//"

---
+++ Adov.Nsudotnet.LinesCounter/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Adov.Nsudotnet.LinesCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.Write("Дядя, программа принимает параметром командной строки тип учитываемых файлов (например, *.cs)");
+                Console.Read();
+                return;
+            }
+            foreach (var file in Directory.GetFiles(Directory.GetCurrentDirectory(), args[0], SearchOption.AllDirectories))
+            {
+                Console.WriteLine(file);
+                int line = File.ReadLines(file).Count(metod);
+                    
+                Console.WriteLine(line);
+            }
+            Console.ReadLine();
+        }
+        static bool metod(string line)
+        {
+            if (line.Trim().StartsWith("//"))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
