b4b4dd247657ae71db6c1a9c490e91aacb3af52b
Dmitriy Adov <36287211+dmitriyadov@users.noreply.github.com>
2018-05-26 05:12:07 +0700

    add /**/

--- Adov.Nsudotnet.LinesCounter/Program.cs
+++ Adov.Nsudotnet.LinesCounter/Program.cs
@@ -18,48 +18,44 @@ namespace Adov.Nsudotnet.LinesCounter
             foreach (var file in Directory.GetFiles(Directory.GetCurrentDirectory(), args[0], SearchOption.AllDirectories))
             {
                 int line = File.ReadLines(file).Count(Netod);
-                if (nowWeInCom == false)
-                {
-                    count += line;
-                }
-               // Console.WriteLine(line + " - " + file);
+                count += line;
+
+                Console.WriteLine(line + " - " + file);
             }
             Console.WriteLine("Количество осмысленных строк равно: " + count);
             Console.ReadLine();
         }
 
-        static bool nowWeInCom = false;
+        private static bool nowWeInCom = false;
 
         static bool Netod(string line)
         {
             if (((line.Trim().StartsWith("//")) && (nowWeInCom == false)) || (string.IsNullOrWhiteSpace(line)))
             {
                 return false;
-            }
-            
+            }  
 
             int i = 0;
             while(i < line.Length-1)
             {
-                if ((line[i] == '/') && (line[i + 1] == '*'))
-                {
-                    Console.WriteLine(line[i]);
-                    nowWeInCom = true;
-                }
                 if ((line[i] == '*') && (line[i + 1] == '/'))
                 {
                     nowWeInCom = false;
+                    if (line.EndsWith("*/")) return false;
                 }
-
-
-                
+                if ((line[i] == '/') && (line[i + 1] == '*'))
+                {
+                    nowWeInCom = true;
+                    i++;
+                }     
                 i++;
             }
-            Console.WriteLine(nowWeInCom);
 
-
-
-            return true;
+            if (nowWeInCom == false)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
