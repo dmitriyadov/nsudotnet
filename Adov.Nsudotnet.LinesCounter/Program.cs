2de83361dcfafb96a59ec403b3cb95653d6ce46a
Dmitriy Adov <36287211+dmitriyadov@users.noreply.github.com>
2018-05-26 04:39:41 +0700

    bred

--- Adov.Nsudotnet.LinesCounter/Program.cs
+++ Adov.Nsudotnet.LinesCounter/Program.cs
@@ -14,21 +14,51 @@ namespace Adov.Nsudotnet.LinesCounter
                 Console.Read();
                 return;
             }
+            int count = 0;
             foreach (var file in Directory.GetFiles(Directory.GetCurrentDirectory(), args[0], SearchOption.AllDirectories))
             {
-                Console.WriteLine(file);
-                int line = File.ReadLines(file).Count(metod);
-                    
-                Console.WriteLine(line);
+                int line = File.ReadLines(file).Count(Netod);
+                if (nowWeInCom == false)
+                {
+                    count += line;
+                }
+               // Console.WriteLine(line + " - " + file);
             }
+            Console.WriteLine("Количество осмысленных строк равно: " + count);
             Console.ReadLine();
         }
-        static bool metod(string line)
+
+        static bool nowWeInCom = false;
+
+        static bool Netod(string line)
         {
-            if (line.Trim().StartsWith("//"))
+            if (((line.Trim().StartsWith("//")) && (nowWeInCom == false)) || (string.IsNullOrWhiteSpace(line)))
             {
                 return false;
             }
+            
+
+            int i = 0;
+            while(i < line.Length-1)
+            {
+                if ((line[i] == '/') && (line[i + 1] == '*'))
+                {
+                    Console.WriteLine(line[i]);
+                    nowWeInCom = true;
+                }
+                if ((line[i] == '*') && (line[i + 1] == '/'))
+                {
+                    nowWeInCom = false;
+                }
+
+
+                
+                i++;
+            }
+            Console.WriteLine(nowWeInCom);
+
+
+
             return true;
         }
     }
